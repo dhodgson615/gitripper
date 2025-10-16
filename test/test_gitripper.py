10ff393d046bcf2d45e68488b17d73da3e3e6bea
dhodgson615 <dhodgson0615@gmail.com>
2025-10-16 12:46:50 -0400

    Changed patch module name

--- test/test_gitripper.py
+++ test/test_gitripper.py
@@ -551,12 +551,12 @@ def test_main_init_fails(
     assert e.value.code == 8
 
 
-@patch("gitripper.initialize_repo")
-@patch("gitripper.extract_zip")
-@patch("gitripper.download_zip")
-@patch("gitripper.get_default_branch", side_effect=Exception("API error"))
-@patch("gitripper.check_git_installed")
-@patch("gitripper.parse_github_url")
+@patch("src.gitripper.initialize_repo")
+@patch("src.gitripper.extract_zip")
+@patch("src.gitripper.download_zip")
+@patch("src.gitripper.get_default_branch", side_effect=Exception("API error"))
+@patch("src.gitripper.check_git_installed")
+@patch("src.gitripper.parse_github_url")
 @patch("argparse.ArgumentParser.parse_args")
 @patch("builtins.print")
 def test_main_get_default_branch_fails(
