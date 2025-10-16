5a05aaeca2d4d2d8c22dfce17c26926c57321993
dhodgson615 <dhodgson0615@gmail.com>
2025-10-16 13:38:41 -0400

    Refactor

--- test/test_gitripper.py
+++ test/test_gitripper.py
@@ -297,7 +297,7 @@ def test_main_success_flow(
 
 
 @patch("argparse.ArgumentParser.parse_args")
-@patch("src.gitripper.parse_github_url", side_effect=ValueError("Invalid URL"))
+@patch("src.main.parse_github_url", side_effect=ValueError("Invalid URL"))
 @patch("builtins.print")
 def test_main_invalid_url(
     mock_print: MagicMock,
