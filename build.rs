e182fd9f2bd155fce9cd08491b651855e86e2099
dhodgson615 <dhodgson0615@gmail.com>
2026-01-15 22:01:46 -0500

    Refactor build.rs to simplify imports and enhance readability

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
-use std::{env, fs};
-use std::process::Command;
+use std::{env, fs, process::Command};
+
+use env::var;
+use fs::write;
 
 fn main() {
-    // Attempt to get a short git hash; fall back to empty string
     let git_hash = Command::new("git")
         .args(&["rev-parse", "--short", "HEAD"])
         .output()
@@ -16,7 +17,7 @@ fn main() {
         })
         .unwrap_or_default();
 
-    let out_path = env::var("CARGO_MANIFEST_DIR").unwrap() + "/src/generated.rs";
+    let out_path = var("CARGO_MANIFEST_DIR").unwrap() + "/src/generated.rs";
     let contents = format!("pub const GIT_HASH: &str = \"{}\";\n", git_hash);
-    fs::write(out_path, contents).expect("Unable to write generated.rs");
+    write(out_path, contents).expect("Unable to write generated.rs");
 }
