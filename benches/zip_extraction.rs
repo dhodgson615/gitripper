472dfed08377e9a6b0160c3ca258a16a3c76846a
dhodgson615 <dhodgson0615@gmail.com>
2026-09-20 14:52:00 -0400

    Specify type explicitly

--- benches/zip_extraction.rs
+++ benches/zip_extraction.rs
@@ -53,7 +53,7 @@ fn benchmark_write_large_file(c: &mut Criterion) {
     c.bench_function("write_large_file", |b| {
         b.iter_with_setup(
             || {
-                let temp_dir: TempDir = tempfile::tempdir().unwrap();
+                let temp_dir: TempDir = tempdir().unwrap();
                 (temp_dir.path().to_path_buf(), temp_dir)
             },
             |(path, _temp_dir)| {
