f5974c0fa45e087d45f2ddcdbed337beb10494e7
dhodgson615 <dhodgson0615@gmail.com>
2026-09-20 14:51:18 -0400

    Add import

--- benches/zip_extraction.rs
+++ benches/zip_extraction.rs
@@ -89,7 +89,7 @@ fn benchmark_write_directory(c: &mut Criterion) {
     c.bench_function("write_directory", |b| {
         b.iter_with_setup(
             || {
-                let temp_dir = tempfile::tempdir().unwrap();
+                let temp_dir = tempdir().unwrap();
                 (temp_dir.path().to_path_buf(), temp_dir)
             },
             |(path, _temp_dir)| {
