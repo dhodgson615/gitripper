575fb8fccf65d9585ac0599a322b306bc7e49e38
dhodgson615 <dhodgson0615@gmail.com>
2026-09-20 14:52:32 -0400

    Specify type explicitly

--- benches/zip_extraction.rs
+++ benches/zip_extraction.rs
@@ -94,7 +94,7 @@ fn benchmark_write_directory(c: &mut Criterion) {
     c.bench_function("write_directory", |b| {
         b.iter_with_setup(
             || {
-                let temp_dir = tempdir().unwrap();
+                let temp_dir: TempDir = tempdir().unwrap();
                 (temp_dir.path().to_path_buf(), temp_dir)
             },
             |(path, _temp_dir)| {
