17a2e6049cc9108c236d2a5b6f68c9fe62731f8a
dhodgson615 <dhodgson0615@gmail.com>
2026-09-20 14:46:09 -0400

    Add type

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -39,7 +39,7 @@ fn integration_reject_invalid_urls() {
 
 #[test]
 fn integration_parse_github_url_with_special_chars() {
-    let urls = vec![
+    let urls: Vec<(&str, &str, &str)> = vec![
         ("https://github.com/user-name/repo-name", "user-name", "repo-name"),
         ("https://github.com/user_123/repo_test", "user_123", "repo_test"),
     ];
